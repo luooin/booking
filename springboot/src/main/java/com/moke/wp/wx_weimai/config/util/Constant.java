6ecef9085067ed8fdf468e261674b0fd95668105
luocong <2369303335@qq.com>
2024-07-11 17:27:28 +0800

    隐私问题

--- springboot/src/main/java/com/moke/wp/wx_weimai/config/util/Constant.java
+++ springboot/src/main/java/com/moke/wp/wx_weimai/config/util/Constant.java
@@ -15,9 +15,9 @@ public interface Constant {
     // 请求的网址
     public static final String WX_LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session";
     // 你的appid
-    public static final String WX_LOGIN_APPID = "wx1d97c8dc4066106e";
+    public static final String WX_LOGIN_APPID = "wx1d*****06e";
     // 你的密匙
-    public static final String WX_LOGIN_SECRET = "d4cf81e9ec27224090d5b708d82e6b5e";
+    public static final String WX_LOGIN_SECRET = "d4cf81e*****e6b5e";
     // 固定参数
     public static final String WX_LOGIN_GRANT_TYPE = "authorization_code";
 
@@ -25,7 +25,7 @@ public interface Constant {
     * 腾讯地图
      */
     //key
-    public static  final String  QQ_MAP_KEY = "XXOBZ-PKQWU-26BVI-4RPT3-LV5HQ-OIBQF";
+    public static  final String  QQ_MAP_KEY = "XXOBZ*****-OIBQF";
     //计算坐标距离
     public static final String QQ_MAP_DISTANCE = "https://apis.map.qq.com/ws/distance/v1/?parameters";
     //搜索城市id
@@ -41,10 +41,10 @@ public interface Constant {
     //api
     public static final String REBOOT_URL = "http://openapi.tuling123.com/openapi/api/v2";
     //apikey
-    public static final String REBOOT_KEY = "469f6180d21646f8b99bd96797c11ece";
+    public static final String REBOOT_KEY = "469f618****6797c11ece";
 
     //百度地图
     public static final String BAIDU_MAP_TO = "http://api.map.baidu.com/geocoding/v3/";
-    public static final String BAIDU_MAP_AK = "qimDWkoOflbIZd01GrBOGwrfDNlwUj8S";
+    public static final String BAIDU_MAP_AK = "qimDWko****DNlwUj8S";
 
 }
